84c90dce8fa97a4cc78992e269e86f47bfd4d081
forest-vulpes <bjy0212@outlook.com>
2025-10-24 01:38:17 +0900

    initialize project

---
+++ Client/mail-client/src/main.js
@@ -0,0 +1,27 @@
+import './style.css'
+
+const dom = {};
+
+document.addEventListener("DOMContentLoaded", (ev) => {
+});
+
+function SyncMailList() { }
+
+/** @param {string} id */
+function GetMailContent(id) { }
+
+function PostMail() { }
+
+/**
+ * @param {string} title
+ * @param {string} content
+ */
+function Notify(title, content) { }
+
+/** format json object into mail list item dom element
+ * @param {object} json
+*/
+function MailListItem(json) { }
+
+/** @param {object} json */
+function ViewMailContent(json) { }
